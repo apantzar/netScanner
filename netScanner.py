ddb82a25a4e3a84214bf4c4b1e9700db1d4e2861
apantzar <apantzar@csd.auth.gr>
2021-01-31 22:33:07 +0200

    V1.0.1a

--- netScanner.py
+++ netScanner.py
@@ -1,6 +1,44 @@
 #!/usr/bin/env python
 import scapy.all as scapy
+import subprocess
 
-def scan():
-   arpRequest = scapy.ARP()
-   print (arpRequest.summary())
+
+class color:
+    BLUE = '\033[96m'
+    DEFAULT = '\033[0m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+
+
+###############packet : arp request directed to broadcast MAC asking for IP########################
+def scanner(ip):
+    arpRequest = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arpRequestBroadcastComb = broadcast / arpRequest  # new packet combines both arpRequest & Broadcast
+
+    ########################Sends packet & Recieves response############################################
+    answer = scapy.srp(arpRequestBroadcastComb, timeout=1, verbose=False)[0]
+    print("IP\t\t\tMAC\n-------------------------------------------------------")
+    for element in answer:
+        print(color.GREEN + "IP: " + color.DEFAULT + element[1].psrc + "\t\t" + color.RED + "MAC: " + color.DEFAULT +
+              element[1].hwsrc)
+
+        print("-------------------------------------------------------")
+
+
+####################################MAIN########################################
+
+print(color.RED + """\
+888  ,d8   ,d8PPPP 888888888 88888888  doooooo ,8b.     888  ,d8 888  ,d8   ,d8PPPP   ,dbPPPp 
+888_dPY8   d88ooo     '88d   88ooooPp  d88     88'8o    888_dPY8 888_dPY8   d88ooo    d88ooP' 
+8888' 88 ,88'        '888           d8 d88     88PPY8.  8888' 88 8888' 88 ,88'      ,88' P'   
+Y8P   Y8 88bdPPP   '88p      8888888P  d888888 8b   `Y' Y8P   Y8 Y8P   Y8 88bdPPP   88  do  
+""" + color.DEFAULT)
+
+print(color.BLUE + "[*] Installing/Updating scapy.." + color.DEFAULT)
+subprocess.call("apt install scapy", shell=True);
+print("")
+print(color.BLUE + "[*] Running 'route -n' for you.." + color.DEFAULT)
+subprocess.call("sudo route -n", shell=True);
+print("")
+scanner(raw_input(color.BLUE + "Give ip or range to scan: " + color.DEFAULT))
